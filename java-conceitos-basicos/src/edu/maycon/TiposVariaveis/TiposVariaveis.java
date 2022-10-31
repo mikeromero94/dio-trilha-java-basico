08cb699a3758ef042a4051d397ceec9e6786acdc
mikeromero94 <maycon.romeros@gmail.com>
2022-10-31 14:35:41 -0300

    Adicionado pacote Tipos variaveis que contem exemplos de variaveis disponiveis no java

---
+++ java-conceitos-basicos/src/edu/maycon/TiposVariaveis/TiposVariaveis.java
@@ -0,0 +1,23 @@
+package edu.maycon.TiposVariaveis;
+
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        double salarioMinimo = 2500.33;
+        String meuNome = "Maycon Romero";
+        Character caractereUnico = 'a';
+        byte ano = 120;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 4;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        
+    }
+
+}
